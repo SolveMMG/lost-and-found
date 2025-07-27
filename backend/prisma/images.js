d25aff5b99f929095b8b63333be61b70429d5ccb
Michael Gacheru <mikachupichu@gmail.com>
2025-07-27 19:01:15 +0300

    Trying to get images to show

---
+++ backend/prisma/images.js
@@ -0,0 +1,6 @@
+module.exports = {
+  iphone14pro: './images/iphone14pro.jpeg',
+  brownleatherwallet: './images/brownleatherwallet.jpeg',
+  bluedenimjacket: './images/bluedenimjacket.jpeg',
+  housekeys: './images/housekeys.jpeg'
+};
